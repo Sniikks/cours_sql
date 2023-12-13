d0430411fe509851f03555eaf74778ce529e94d5
Sniikks <snikspro@hotmail.com>
2023-12-13 09:19:51 +0100

    Commit all

---
+++ magasin.sql
@@ -0,0 +1,67 @@
+/*Créer une base de donnée qui ce nomme magasin et qui posséde trois table
+
+client avec 6 colonne
+id int
+prenom string
+nom string
+email string
+ville string
+password string
+ 
+ 
+ 
+ 
+ 
+ */
+
+-- Création de la base de données
+CREATE DATABASE IF NOT EXISTS magasin;
+
+-- Utilisation de la base de données
+USE magasin;
+
+-- Création de la table "client"
+CREATE TABLE IF NOT EXISTS client (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(255),
+    nom VARCHAR(255),
+    email VARCHAR(255),
+    ville VARCHAR(255),
+    password VARCHAR(255)
+);
+
+INSERT INTO client (prenom, nom, email, ville, password) VALUES 
+('Bernard', 'Lermitte', 'lermitte@null', 'null', 'bernardo'), 
+('Clautide', 'Lafond', 'clauclau@null', 'null', 'clau1234');
+
+-- Création de la table "commande"
+CREATE TABLE IF NOT EXISTS commande (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    client_id INT(11) NOT NULL,
+    date_achat DATETIME NOT NULL,
+    reference VARCHAR(255) NOT NULL,
+    prix_total FLOAT NOT NULL 
+);
+
+INSERT INTO commande (client_id, date_achat, reference, prix_total) VALUES
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100),
+(1, '2023-12-12 10:10:10', '001471', 100);
+
+-- Création de la table "produit"
+CREATE TABLE IF NOT EXISTS produit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255),
+    quantite DECIMAL(10, 2),
+    prix DECIMAL(10, 2)
+);
+
